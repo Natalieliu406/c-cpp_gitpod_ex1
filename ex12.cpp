c1c3f3d74175c2e3bfd70fd4315773f40e404ad7
Natalieliu406 <100347589+Natalieliu406@users.noreply.github.com>
2022-04-14 20:49:58 +0800

    Create ex12.cpp

---
+++ ex12.cpp
@@ -0,0 +1,19 @@
+//int 1[9] = (1,2,3,5,6,9,8,7,4},
+int a[3]={5,6,2);
+int 1,j:
+for (1=0;1
+<2;1++
+for (j=0;]
+< 2;j++)
+if(a[j] > alj+11){
+int temp
+= a[j+113
+a[j+1] = aljl:
+a[j] = temp;
+}
+]
+for (1-0;13;i+){
+cout«<
+"al"
+}
+cout «< endl:
